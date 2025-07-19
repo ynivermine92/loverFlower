a37760fe938a5d02154156a73680eb0ba47a7c6a
ynivermine92 <ynivermine92@gmail.com>
2025-07-19 17:58:08 +0300

    v2

--- build/js/scripts.min.js
+++ build/js/scripts.min.js
@@ -62,13 +62,17 @@ window.addEventListener('DOMContentLoaded', () => {
         const search = document.querySelector('.search');
         const searchImage = document.querySelector('.menu__search');
 
+        const sidebar = document.querySelector('.sidebar');
+
 
         searchBtn.addEventListener('click', () => {
             search.classList.remove('active')
+            sidebar.style.zIndex = '1';
         })
         searchImage.addEventListener('click', () => {
             search.classList.add('active')
-
+            sidebar.classList.add('active')
+            sidebar.style.zIndex = '-1';
         })
     }
     search();
