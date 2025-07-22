76176a7fe5e502a685eb52dc45ad4bd8844d71d6
ynivermine92 <ynivermine92@gmail.com>
2025-07-22 14:14:36 +0300

    v3

--- src/js/libs/header.js
+++ src/js/libs/header.js
@@ -1,4 +1,6 @@
 
+const userBlock = document.querySelector('.header__user-items');
+
 function burgerMenu() {
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu')
@@ -16,6 +18,7 @@ function burgerMenu() {
             homeTitle.style.display = 'none';
             cart.style.display = 'none'
             navbar.classList.add('active')
+            userBlock.style.display = 'none'
         } else {
             menu.classList.remove('active')
             burger.classList.remove('active')
@@ -23,38 +26,59 @@ function burgerMenu() {
             navbar.classList.remove('active')
             homeTitle.style.display = 'block';
             cart.style.display = 'block'
+            userBlock.style.display = 'flex'
         }
     })
+
 
     window.addEventListener('resize', () => {
         if (window.innerWidth > 991.98) {
             menu.classList.remove('active')
             burger.classList.remove('active')
             body.classList.remove('locked')
-
-
-
+            userBlock.classList.remove('active')
+        } else {
+            userBlock.classList.add('active')
         }
     })
+
+    if (window.innerWidth > 991.98) {
+        menu.classList.remove('active')
+        burger.classList.remove('active')
+        body.classList.remove('locked')
+        userBlock.classList.remove('active')
+    } else {
+        userBlock.classList.add('active')
+    }
 }
+
+
 burgerMenu()
 
 
 function fixedHeader() {
     const sidebar = document.querySelector('.sidebar')
     const nav = document.querySelector('.header')
-    const userBlock = document.querySelector('.header__user-items')
 
 
     const breakpoint = 1
     if (window.scrollY >= breakpoint) {
         nav.classList.add('fixed')
         sidebar.style.opacity = 0;
+
         userBlock.classList.add('active')
+
+
     } else {
         nav.classList.remove('fixed')
         sidebar.style.opacity = 1;
-        userBlock.classList.remove('active')
+        if (window.innerWidth > 991.98) {
+            userBlock.classList.remove('active')
+        }
+
+
+
+
     }
 }
 window.addEventListener('scroll', fixedHeader)
